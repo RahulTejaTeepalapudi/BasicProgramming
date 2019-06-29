bc68e84b94454b1aaee5e07e938a00c6a1bee63f
Rahul Teja <me.rahulteja@outlook.com>
2019-06-29 22:07:25 +0530

    Modified Code for Duration Problem. Added a new problem Ladderophilia. Passed at Hackerearth.

--- Duration/Program.cs
+++ Duration/Program.cs
@@ -7,12 +7,24 @@ namespace Duration
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            while (n > 0)
+
+            for (int i = 0; i < n; i++)
             {
-                int[] N = Array.ConvertAll(Console.ReadLine().Split(' '), e => int.Parse(e));
-                int d = (N[2] * 60 + N[3]) - (N[0] * 60 + N[1]);
-                Console.WriteLine(d);
-                n--;
+                var input = Console.ReadLine();
+                string[] array = input.Split(' ');
+
+                int sh = int.Parse(array[0]);
+                int sm = int.Parse(array[1]);
+                int eh = int.Parse(array[2]);
+                int em = int.Parse(array[3]);
+
+                TimeSpan start = new TimeSpan(sh, sm, 0);
+                TimeSpan end = new TimeSpan(eh, em, 0);
+
+                TimeSpan diff = end.Subtract(start);
+
+                Console.WriteLine(diff.Hours + " " + diff.Minutes);
+
             }
         }
     }
---
+++ Ladderophilia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ladderophilia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                while (count <= 2)
+                {
+                    if (count == 0 || count == 1)
+                    {
+                        Console.WriteLine("*   *");
+                    }
+                    if (count == 2 && i != n)
+                    {
+                        Console.WriteLine("*****");
+                    }
+                    count++;
+                }
+                count = 0;
+            }
+            Console.ReadLine();
+        }
+    }
+}
